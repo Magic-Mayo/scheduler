8b63bf259dd1bf7c411a35b69c69293c95c65593
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-03 23:58:43 -0700

    init files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/under-the-weather";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+require('./routes/index')(app);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
