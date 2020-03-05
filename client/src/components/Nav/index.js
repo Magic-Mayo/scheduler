2703af6c0956b24edc1fac5d49c6c654c9dac878
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-05 16:08:08 -0700

    changed box shadow to rgba so opacity could be changed

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     const [expanded, setExpanded] = useState(true);
 
     return (
-        <Nav onClick={() => setExpanded(!expanded)} Width={expanded ? '200px' : '50px'} boxShadow={expanded ? 'box-shadow: #333 12px 5px 15px;' : 'box-shadow: #333 3px 5px 15px;'}>
+        <Nav onClick={() => setExpanded(!expanded)} Width={expanded ? '200px' : '50px'} boxShadow={expanded ? 'box-shadow: rgba(0, 0, 0, .6) 20px 5px 15px 10px;' : 'box-shadow: #333 3px 5px 15px;'}>
             <span >{expanded ? '<' : '>'}</span>
             <Link to='/' margin='0 0 0 80px;'>
                 Home
