c4e26464acdb92a9925d85f397afd891afbc9a33
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-05 14:26:03 -0700

    removed expanded prop from Link

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <Nav onClick={() => setExpanded(!expanded)} Width={expanded ? '200px' : '50px'} boxShadow={expanded ? 'box-shadow: #333 12px 5px 15px;' : 'box-shadow: #333 3px 5px 15px;'}>
             <span >{expanded ? '<' : '>'}</span>
-            <Link to='/' margin='0 0 0 80px;' expanded={expanded}>
+            <Link to='/' margin='0 0 0 80px;'>
                 Home
             </Link>
         </Nav>
