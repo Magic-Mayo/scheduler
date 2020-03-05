309bbc516ed5dc6cc29ae588a20273dba2d2ab93
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-05 14:26:03 -0700

    added conditional for display, grid template columns, align items, bg color, box shadow, grid column, and font size. added in hover for spans that control what month is selected

--- client/src/components/styledComponents/lowLevel/wrapper/index.js
+++ client/src/components/styledComponents/lowLevel/wrapper/index.js
@@ -1,12 +1,25 @@
 import styled from 'styled-components';
 
 export default styled.div`
-    display: flex;
+    display: ${props => props.display || 'flex'};
+    ${props => props.display === 'grid' ? 
+    `grid-template-columns: repeat(7, 1fr)`
+    : ''};
+    ${props => props.rows ? `` : ''}
     flex-direction: ${props => props.flexDirection || 'column'};
     justify-content: ${props => props.justifyContent || 'center'};
-    align-items: ${props => props.flexDirection ? '' : 'center'};
+    align-items: ${props => props.alignItems ? props.alignItems : props.flexDirection ? '' : 'center'};
     width: ${props => props.Width || ''};
     height: ${props => props.Height || ''};
     border: ${props => props.border || ''};
-    background-color: ${props => props.bgColor || ''}
+    background-color: ${props => props.bgColor || ''};
+    box-shadow: ${props => props.boxShadow || ''};
+    grid-column: ${props => props.gridColumn || ''};
+    ${props => props.fontSize ? `font-size: ${props.fontSize};` : '' }
+
+    &.calendar-month {
+        span:first-of-type:hover, span:last-of-type:hover {
+            cursor: pointer
+        }
+    }
 `
