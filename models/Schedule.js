bfca628d1ea92de27004c4a9dbb7bada406ae252
MagicMayo <mike.mayoii@gmail.com>
2020-03-05 22:01:59 -0700

    made date into array and added month array

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScheduleSchema = new Schema({
-    date: {type: String, require: true},
-    time: {type: String, require: true},
+    date: [{
+        month: [{type: String, require: true}]
+    }],
+    time: [{type: String, require: true}],
     name: {type: String, require: true},
     email: {type: String, require: true}
 })
