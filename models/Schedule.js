7ccaa67a094955a30e9a201bcac392764c58208a
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-06 02:10:41 -0700

    added months, days, and times arrays and id to store the staff id

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScheduleSchema = new Schema({
-    date: [{
-        month: [{type: String, require: true}]
+    months: [{
+        month: {type: String, require: true},
+        days: [{
+            date: {type: Date, require: true},
+            times: [{
+                time: {type: String, require: true},
+                studentName: {type: String, require: true},
+                studentEmail: {type: String, require: true}
+            }]
+        }]
     }],
-    time: [{type: String, require: true}],
     name: {type: String, require: true},
-    email: {type: String, require: true}
+    id: {type: Number, require: true}
 })
 
 const Schedule = mongoose.model('Schedule', ScheduleSchema);
