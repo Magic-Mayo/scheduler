770111565afe9f405bbe5b6e549e4dad9fdeea7e
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-06 02:10:40 -0700

    added in justify content, display, padding, and conditional render of active

--- client/src/components/styledComponents/lowLevel/button/index.js
+++ client/src/components/styledComponents/lowLevel/button/index.js
@@ -4,15 +4,21 @@ export default styled.button`
     background-color: ${props => props.bgColor || '#172a55'};
     width: ${props => props.Width || '125px'};
     height: ${props => props.Height || '50px'};
-    color: ${props => props.color || '#fff'};
+    color: ${props => props.Color || '#fff'};
     border-radius: ${props => props.borderRadius || '7px'};
     border: ${props => props.border || 'none'};
     box-shadow: ${props => props.boxShadow ? '#444 7px 5px 15px' : ''};
     font-size: 18px;
+    justify-content: ${props => props.justifyContent || ''};
+    display: ${props => props.display || ''};
+    padding: 8px;
+    vertical-align: text-top;
 
-    &:active {
-        box-shadow: none;
-        border-radius: 7px;
+    ${props => !props.calendar ?
+        `&:active {
+            box-shadow: none;
+            border-radius: 7px;
+        }` : ''
     }
 
     &:focus {
