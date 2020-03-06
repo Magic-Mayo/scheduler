9ea7b4cb15ff47710ba17aebfae49f03b4c3cc37
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-06 02:12:53 -0700

    added staff for each student

--- models/Students.js
+++ models/Students.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const StudentsSchema = new Schema({
     name: {type: String, require: true},
-    email: {type: String, require: true}
+    email: {type: String, require: true},
+    staff: [{
+        id: {type: String, require: true},
+        name: {type: String, require: true},
+    }]
 })
 
-const Students = mongoose.model('Students', StudentsSchema);
+const Students = mongoose.model('Student', StudentsSchema);
 
 module.exports = Students;
