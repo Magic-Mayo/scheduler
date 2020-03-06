709d91f8c185ca8e46419774f31bc0fbe6dd83cf
MagicMayo <mike.mayoii@gmail.com>
2020-03-05 22:01:56 -0700

    formatting and removed async from useeffect. set value of each day to its date

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -10,6 +10,7 @@ import {
     subMonths
 } from 'date-fns';
 import { Wrapper, Button } from '../styledComponents';
+import axios from 'axios';
 
 const days = [
     'Sun',
@@ -25,13 +26,13 @@ const days = [
 const Calendar = () => {
     const [calendar, setCalendar] = useState();
     const [selectedMonth, setSelectedMonth] = useState(new Date());
+    const [availabilty, setAvailability] = useState();
 
     const dateSelect = e => {
-
+        
     }
 
     const getDays = () => {
-
         return (
             days.map(day => (
                 <Wrapper key={day}>
@@ -51,26 +52,40 @@ const Calendar = () => {
         const endofWeek = endWeek(endOfMonth);
 
         let currentDay = startOfWeek;
-        console.log(endofWeek)
         let numberedDate;
 
         while(currentDay <= endofWeek){
             for(let i = 0; i < 7; i++){
                 numberedDate = dateFormat(currentDay, 'd');
-                const day = currentDay;
                 days.push(
-                    <Button key={currentDay} onClick={dateSelect} color='#000' borderRadius='0' border='.5px solid #ccc' Height='100%' Width='100%' bgColor='#fff'>{numberedDate}</Button>
+                    <Button
+                    calendar
+                    value={dateFormat(currentDay, 'MMMM d, yyyy')}
+                    key={currentDay}
+                    onClick={dateSelect}
+                    color='#000' borderRadius='0'
+                    border='.5px solid #ccc'
+                    Height='100%'
+                    Width='100%'
+                    bgColor='#fff'
+                    >{numberedDate}</Button>
                 )
 
                 currentDay = addDays(currentDay, 1);
             }
 
             weeks.push(
-                <Wrapper flexDirection='row' Height='100%' gridColumn='1/8' justifyContent='space-between'>
+                <Wrapper
+                key={dateFormat(currentDay, 'w')}
+                flexDirection='row'
+                Height='100%'
+                gridColumn='1/8'
+                justifyContent='space-between'
+                borderLeft='#000 solid 1px'
+                borderRight='#000 solid 1px'
+                >
                     {days.map(day => (
-                        <>
-                            {day}
-                        </>
+                        day
                     ))}
                 </Wrapper>
                 );
@@ -78,19 +93,23 @@ const Calendar = () => {
             days.length = 0;
         }
 
-        console.log(weeks)
-
         return weeks.map(week => (
             week
         ))
         
     }
-        
+
+    useEffect(() => {
+        axios.get(`/availability/${dateFormat(selectedMonth, 'MMMM%20yyyy')}`).then(newMonth =>
+            setAvailability(newMonth.data)
+        )
+    },[selectedMonth])
+    
     return (
         <Wrapper Width='80%' border='solid 2px #666' Height='60%' display='grid' boxShadow='#ccc 10px 10px 15px'>
             <Wrapper className='calendar-month' gridColumn='1/8' flexDirection='row' justifyContent='space-around' fontSize='200%'>
                 <span onClick={() => setSelectedMonth(subMonths(selectedMonth, 1))}>&lt;</span>
-                <span>{dateFormat(selectedMonth, 'LLLL')}</span>
+                <span>{dateFormat(selectedMonth, 'LLLL yyyy')}</span>
                 <span onClick={() => setSelectedMonth(addMonths(selectedMonth, 1))}>&gt;</span>
             </Wrapper>
             {getDays()}
