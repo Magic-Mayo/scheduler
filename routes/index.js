cc49b9048011cb2c4ac5daf172df122416fe02f3
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-07 08:51:37 -0700

    removed slash from end of first route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const controller = require('../controllers')
 
 module.exports = (app) => {
-    app.get('/admin/:id/students/', controller.addNewStudents);
+    app.get('/admin/:id/students', controller.addNewStudents);
     app.get('/student/:email', controller.findOne);
     app.get('/availability/:date/:id', controller.getAvailability);
     app.put('/availability/:date/:id', controller.updateAvailability);
