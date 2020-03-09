2529c013c95728d5869b010fc48e66888ddde178
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-09 11:14:41 -0700

    wrapped components in context provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,42 @@
-import React, {useContext} from 'react';
-import {Switch, Route, useLocation} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Switch, Route, useLocation, Redirect} from 'react-router-dom';
 import Student from './pages/Student';
 import Admin from './pages/Admin';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Calendar from './components/Calendar';
 import {Wrapper} from './components/styledComponents';
+import {UserContext} from './UserContext';
 
 function App() {
+    const [user, setUser] = useState();
     let location = useLocation();
+
     return (
         <Wrapper flexDirection='row' Width='100vw' Height='100vh' justifyContent='flex-start'>
             {location.pathname !== '/' &&
                 <Nav />
             }
-            <Route exact path='/'>
-                <Home />
-            </Route>
-            <Switch>
-                <Route path='/admin'>
-                    <Admin />
+            <UserContext.Provider value={{user, setUser}}>
+                <Route exact path='/'>
+                    <Home />
                 </Route>
-                <Route path='/student'>
-                    <Student />
+                <Switch>
+                    <Route path='/admin'>
+                        <Admin />
+                    </Route>
+                    <Route exact path='/student'>
+                        <Student />
+                    </Route>
+                </Switch>
+                <Route exact path='/student/calendar/'>
+                    {user ?
+                        <Calendar />
+                    :
+                        <Redirect to='/student' />
+                    }
                 </Route>
-            </Switch>
-            <Route exact path='/calendar/:date?'>
-                <Calendar />
-            </Route>
+            </UserContext.Provider>
         </Wrapper>
     )
 }
