76791ac33409f10f8228364706244fd426be99a1
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-10 01:02:28 -0700

    started making useeffects to store user in localstorage until cookies or jwt can be implemented. wrapped everything except top level wrapper in instructor and user context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Switch, Route, useLocation, Redirect} from 'react-router-dom';
 import Student from './pages/Student';
 import Admin from './pages/Admin';
@@ -6,37 +6,48 @@ import Home from './components/Home';
 import Nav from './components/Nav';
 import Calendar from './components/Calendar';
 import {Wrapper} from './components/styledComponents';
-import {UserContext} from './UserContext';
+import {UserContext, InstructorContext} from './Context';
 
 function App() {
     const [user, setUser] = useState();
+    const [instructor, setInstructor] = useState([{name: 'me', id: 1}, {name: 'you', id: 2}]);
     let location = useLocation();
 
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user));
+    }, [user]);
+
+    // useEffect(() => {
+    //     if(localStorage.getItem('user')) setUser(localStorage.getItem('user'));
+    // }, [])
+
     return (
-        <Wrapper flexDirection='row' Width='100vw' Height='100vh' justifyContent='flex-start'>
-            {location.pathname !== '/' &&
-                <Nav />
-            }
-            <UserContext.Provider value={{user, setUser}}>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Switch>
-                    <Route path='/admin'>
-                        <Admin />
+        <Wrapper flexDirection='row' w='100vw' h='100vh' justifyContent='flex-start'>
+            <InstructorContext.Provider value={{instructor, setInstructor}}>
+                <UserContext.Provider value={{user, setUser}}>
+                {location.pathname !== '/' &&
+                    <Nav />
+                }
+                    <Route exact path='/'>
+                        <Home />
                     </Route>
-                    <Route exact path='/student'>
-                        <Student />
-                    </Route>
-                </Switch>
-                <Route exact path='/student/calendar/'>
-                    {user ?
-                        <Calendar />
-                    :
-                        <Redirect to='/student' />
-                    }
-                </Route>
-            </UserContext.Provider>
+                    <Switch>
+                        <Route path='/admin'>
+                            <Admin />
+                        </Route>
+                        <Route exact path='/student'>
+                            <Student />
+                        </Route>
+                    </Switch>
+                        <Route path='/student/calendar/:instructorId?/:date?'>
+                            {user ?
+                                <Calendar />
+                                :
+                                <Redirect to='/student' />
+                            }
+                        </Route>
+                </UserContext.Provider>
+            </InstructorContext.Provider>
         </Wrapper>
     )
 }
