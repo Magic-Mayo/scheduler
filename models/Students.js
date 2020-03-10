7111b5158c8f6c664b62390f8d826d241d98fc7d
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-10 16:54:39 -0700

    added id field

--- models/Students.js
+++ models/Students.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 const StudentsSchema = new Schema({
     name: {type: String, require: true},
     email: {type: String, require: true},
+    id: {type: String, require: true},
     staff: [{
         id: {type: String, require: true},
         name: {type: String, require: true},
     }]
 })
 
-const Students = mongoose.model('Students');
+const Students = mongoose.model('Students', StudentsSchema);
 
 module.exports = Students;
