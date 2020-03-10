fedffd29843045b53f14a86788cf8eff1408fae6
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-10 01:04:14 -0700

    added props

--- client/src/components/styledComponents/lowLevel/wrapper/index.js
+++ client/src/components/styledComponents/lowLevel/wrapper/index.js
@@ -2,23 +2,25 @@ import styled from 'styled-components';
 
 export default styled.div`
     display: ${props => props.display || 'flex'};
-    ${props => props.display === 'grid' ? 
-    `grid-template-columns: repeat(7, 1fr)`
+    ${({grid}) => grid && grid === 'grid' ? `grid-template-columns: repeat(7, 1fr)`
+    : grid === 'grid1' ? `grid-template-columns: repeat(5, 1fr)`
     : ''};
     flex-direction: ${props => props.flexDirection || 'column'};
     justify-content: ${props => props.justifyContent || 'center'};
     align-items: ${props => props.alignItems ? props.alignItems : props.flexDirection ? '' : 'center'};
-    width: ${props => props.Width || ''};
-    height: ${props => props.Height || ''};
+    width: ${({w}) => w || ''};
+    height: ${({h}) => h || ''};
     border: ${props => props.border || ''};
     background-color: ${props => props.bgColor || ''};
     box-shadow: ${props => props.boxShadow || ''};
     grid-column: ${props => props.gridColumn || ''};
-    ${props => props.fontSize ? `font-size: ${props.fontSize}` : '' };
+    ${({fontS}) => fontS ? `font-size: ${fontS}` : '' };
     border-left: ${props => props.borderLeft || ''};
     border-right: ${props => props.borderRight || ''};
     border-bottom: ${props => props.borderBottom || ''};
     margin: ${props => props.margin || ''};
+    position: ${({position}) => position || ''};
+    top: ${({top}) => top || ''};
 
 
     &.calendar-month {
