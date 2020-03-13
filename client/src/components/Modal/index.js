62a170171fe848f04b0ae1dc1e165755b2652a4f
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-13 14:23:47 -0700

    created modal and have it centered on screen

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,1 +1,24 @@
 import React from 'react';
+import {Wrapper} from '../styledComponents';
+
+const Modal = props => {
+    return (
+        <Wrapper
+        position='fixed'
+        margin='0 auto'
+        top='25%'
+        left='50%'
+        transform='translateX(-47%)'
+        w='400px'
+        h='240px'
+        bgColor='#fff'
+        boxShadow='rgba(0,0,0,.7) 0 20px 15px 10px'
+        padding='10px'
+        justifyContent='space-evenly'
+        >
+            {props.children}
+        </Wrapper>
+    )
+}
+
+export default Modal;
