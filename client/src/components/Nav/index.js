57d28eb3b10c3b98d37d055b80ee77278a6cc027
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-14 04:05:38 -0700

    added font awesome icons and changed nav bar widthto 45px when collapsed

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { Nav, Button, P} from '../styledComponents';
 import {InstructorContext, UserContext} from '../../Context';
 import {Link} from 'react-router-dom';
+import {FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome';
 
 const NavBar = () => {
     const [expanded, setExpanded] = useState(false);
@@ -11,52 +12,55 @@ const NavBar = () => {
 
     return (
         <Nav
-        w={expanded ? '200px' : '35px'}
+        w={expanded ? '200px' : '45px'}
         expanded={expanded}
         boxShadow={expanded ? 
             'box-shadow: rgba(0, 0, 0, .6) 25px 5px 15px 10px;' :
             'box-shadow: #333 3px 5px 15px;'
         }>
-            <span
+            <FAIcon
+            size='3x'
+            className='icon'
+            icon={expanded ? 'arrow-left' : 'arrow-right'}
             onClick={() => {setInstructorExpand(false); setExpanded(!expanded)}}
-            >
-                {expanded ? '<' : '>'}
-            </span>
+             />
+
             <P
-            
+            textAlign='left'
             fontS='30px'
             fontW='bold'
             bgColor='#172a55'
             fontColor='#fff'
             margin='15px 0'
-            padding='5px 0 5px 38px'
+            padding='5px 10px 5px 70px'
             >
                 {user ? user.name : 'Please Login'}
             </P>
             <Link
             to='/'
             >
-                <P
-                padding='0 0 0 50px;'
+                <Button
                 h='50px'
-                lineHeight='50px'
                 textAlign='left'
-                fontColor='#fff'
                 fontS='24px'
+                disp='flex'
+                bgColor='inherit'
                 >
-                    Home
-                </P>
+                    <FAIcon icon='home' />
+                    <P fontColor='#fff' fontS='24px' margin='0 0 0 20px' >Home</P>
+                </Button>
             </Link>
             <Button
+            disp='flex'
+            padding='5px'
             w='100%'
-            padding='0 0 0 50px'
-            display='inline-block'
             textAlign='left'
             bgColor='inherit'
             fontS='25px'
             onClick={() => setInstructorExpand(!instructorExpand)}
             >
-                Instructor
+                <FAIcon icon='chalkboard-teacher' />
+                <P fontS='24px' margin='0 0 0 20px'>Instructor</P>
             </Button>
             {instructorExpand &&
                 instructor.map(ins => (
@@ -77,9 +81,10 @@ const NavBar = () => {
             {user &&
                 <Button
                 onClick={() => setUser(null)}
-                margin='0 37px'
+                margin='0 50px'
                 position='absolute'
                 bottom='50px'
+                w='100px'
                 >
                     Logout
                 </Button>
