8102fd93898b08b39704407d01efa9ae288b84f2
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-14 04:06:45 -0700

    renamed admin routes to staff

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const controller = require('../controllers')
 
 module.exports = (app) => {
-    app.post('/admin/:id/getstudents', controller.addNewStudents);
-    app.post('/admin/add', controller.addNewStaff);
+    app.post('/staff/:id/getstudents', controller.addNewStudents);
+    app.post('/staff/add', controller.addNewStaff);
     app.get('/student/:email', controller.findOne);
     app.get('/availability/:date/:id', controller.getAvailability);
     app.put('/availability/:date/:id', controller.updateAvailability);
     app.post('/availability/:date/:id', controller.setAvailability);
-    app.put('/schedule/:id/:date/:time', controller.scheduleTime);
+    app.put('/schedule/:id', controller.scheduleTime);
     // app.get('/student/instructors', controller.)
 }
