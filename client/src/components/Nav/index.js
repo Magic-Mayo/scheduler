bc3d8c15c979d88b2c45ba56469117aca70b1861
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-17 12:54:47 -0700

    added refresh icon to navbar instead of calendar. added student calendar view but commented out until calendar can be used with student view instead of instructor view

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Nav, Button, P} from '../styledComponents';
+import { Nav, Button, P, Wrapper} from '../styledComponents';
 import {InstructorContext, UserContext} from '../../Context';
 import {Link, useLocation} from 'react-router-dom';
 import {FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome';
@@ -7,7 +7,7 @@ import {FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome';
 const NavBar = () => {
     const [expanded, setExpanded] = useState(false);
     const [instructorExpand, setInstructorExpand] = useState(false);
-    const {instructor, loading, setLoading} = useContext(InstructorContext);
+    const {instructor, loading, refresh, setLoading, setRefresh} = useContext(InstructorContext);
     const {user, setUser} = useContext(UserContext);
     const location = useLocation();
 
@@ -43,7 +43,7 @@ const NavBar = () => {
                 {user ? user.name : 'Please Login'}
             </P>
             <Link
-            to={loading ? location.pahtname : '/student'}
+            to={loading ? location.pathname : '/student'}
             >
                 <Button
                 h='50px'
@@ -63,7 +63,7 @@ const NavBar = () => {
             w='100%'
             textAlign='left'
             bgColor='inherit'
-            fontS='25px'
+            fontS='24px'
             onClick={() => {
                 setExpanded(instructor ? true : !expanded && false);
                 setInstructorExpand(instructor ? !instructorExpand : false)
@@ -88,6 +88,44 @@ const NavBar = () => {
                         </P>
                     </Link>
                 ))
+            }
+            {/* <Link
+            to={loading ? location.pathname : '/student/mycalendar'}
+            >
+                <Button
+                bgColor='inherit'
+                padding='5px 0 5px 12px'
+                disp='flex'
+                w='100%'
+                fontS='24px'
+                >
+                    <FAIcon icon='calendar-alt' color='#fff' />
+                    <P
+                    margin='0 0 0 23px'
+                    fontS='24px'
+                    whiteSpace='nowrap'
+                    >
+                        My Calendar
+                    </P>
+                </Button>
+            </Link> */}
+            {location.pathname.split('student/')[1] &&
+                <Button
+                w='100%'
+                disp='flex'
+                flexDirection='row'
+                padding='5px 8px'
+                fontS='24px'
+                bgColor='inherit'
+                onClick={() => setRefresh(!refresh)}
+                >
+                    <FAIcon
+                    color='#fff'
+                    className='refresh'
+                    icon='sync'
+                    />
+                    <P margin='0 0 0 23px' fontS='24px'>Refresh</P>
+                </Button>
             }
             {user &&
                 <Button
