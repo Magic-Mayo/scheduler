f4fa6a0aef67da8e0ed1dec5827243c94df59bbd
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-18 01:23:32 -0700

    added theme context for future use

--- client/src/Context.js
+++ client/src/Context.js
@@ -3,3 +3,4 @@ import {createContext} from 'react';
 export const UserContext = createContext(null);
 export const InstructorContext = createContext(null);
 export const CurrentInstructorContext = createContext(null);
+export const ThemeContext = createContext(null);
