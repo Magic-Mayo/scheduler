ae532f9dd5c047bae72e2d27d0a4e8e42d0dc3a9
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-18 01:29:00 -0700

    made file for email function

---
+++ controllers/email.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const mail = require('nodemailer');
+const {format} = require('date-fns');
+
+
+module.exports = {
+    sendConfirmation: async (studentName, studentEmail, staffName, staffEmail, time, topic) => {
+        const transport = mail.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASS
+            }
+        })
+
+        const message = {
+            from: 'Virtual Office Hours',
+            replyTo: staffEmail,
+            to: 'mike.mayoii@gmail.com',
+            cc: staffEmail,
+            subject: `Office Hours Scheduled!`,
+            text: `${studentName}, you have been scheduled for virtual office hours with ${staffName} on ${format(new Date(time), 'MMMM d, yyyy hh:mm a')}.  You will be discussing "${topic}".  If you feel this has been scheduled in error please respond to this email and I will delete the time for you!`,
+        };
+
+        transport.sendMail(message, (err, res)=>{
+            if(err){
+                console.error(err)
+            }
+
+        })
+    }
+}
