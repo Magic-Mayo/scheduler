2abcf73001f4d6af277de472554a58dccb9b34dd
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-25 16:18:59 -0700

    removed morgan and console log

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
-const morgan = require('morgan')
+
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scheduler';
 mongoose.connect(MONGODB_URI, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -15,8 +14,6 @@ app.use(express.static("public"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-
-console.log(process.env)
 
 require('./routes/index')(app);
 
