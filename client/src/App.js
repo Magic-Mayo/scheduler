d7e2856592c1448b57ea2364544fd47cf1b9484c
Magic-Mayo <mike.mayoii@gmail.com>
2020-03-25 18:57:43 -0700

    added switch over all routes to add in catch all

--- client/src/App.js
+++ client/src/App.js
@@ -60,6 +60,7 @@ function App() {
                     {location.pathname !== '/' &&
                         <Nav />
                     }
+                    <Switch>
                         <Route exact path='/'>
                             <Redirect to='/student' />
                             {/* <Home /> */}
@@ -88,7 +89,8 @@ function App() {
                                 }
                             </Route>
                         </Switch>
-                        <Route path='/*'><Redirect to='/' /></Route>
+                        <Redirect to='/' />
+                    </Switch>
                     </UserContext.Provider>
                 </CurrentInstructorContext.Provider>
             </InstructorContext.Provider>
